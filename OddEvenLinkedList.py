6d7ed862b0313f34a6669da045013dda05d249f1
mohitsinghnegi1 <37727115+mohitsinghnegi1@users.noreply.github.com>
2020-07-19 01:58:04 +0530

    leetcode, eventloop, even driven application, promises

---
+++ OddEvenLinkedList.py
@@ -0,0 +1,53 @@
+
+# Qus:https://leetcode.com/problems/odd-even-linked-list/
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def oddEvenList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        #case if there are 2 or less node then return
+        if(head==None or head.next==None or head.next==None):
+            return head
+        
+        #keep ptr1 at odd position
+        ptr1=head
+        
+        #keep ptr2 at even position
+        ptr2=head.next
+        
+        #this pointer will be use as a ptr1's next node
+        temp=head.next
+        
+
+        while(ptr1.next!=None and ptr1.next.next!=None):
+            
+            #point ptr1.next in next odd position and move ptr1 at that position
+            ptr1.next=ptr1.next.next
+            ptr1=ptr1.next
+            
+            #check if ptr2.next.next location is not None
+            #if that's the case then set its next as None 
+            #bec it will be the last even node
+            if(ptr2.next==None or ptr2.next.next==None):
+                ptr2.next=None
+                break
+            
+            #if next even position is available the set ptr2.next to that node
+            #move ptr2 to that node
+            ptr2.next=ptr2.next.next
+            ptr2=ptr2.next
+            #print ptr2.val
+            
+        #at the end assign pt1.next to that 2nd node 
+        ptr1.next=temp
+        
+    
+        return head
---
+++ nodejs.js
@@ -0,0 +1,41 @@
+// const main = () => {
+//   console.log('print1');
+
+//   //call stack->web api -> exceutes -> pass to event queue -> once call stack gets empty this function gets executed
+//   setTimeout(() => {
+//     console.log('callback');
+//   }, 0);
+//   console.log('print2');
+// };
+
+// main();
+
+//how to use promises
+
+console.log('first');
+
+const doSomething = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('function exected after 1 s');
+      resolve('data');
+    }, 1000);
+  });
+};
+
+doSomething()
+  .then((data) => {
+    console.log(data);
+    // throw Error('something went worng');
+    return new Promise((resolve) => {
+      resolve(data + '1');
+    });
+  })
+  .then((data1) => {
+    console.log(data1);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+console.log('last');
