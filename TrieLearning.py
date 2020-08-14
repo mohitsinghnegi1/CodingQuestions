87422a8826b131eafa0023006695b783606b5a96
mohitsinghnegi1 <37727115+mohitsinghnegi1@users.noreply.github.com>
2020-08-15 01:29:33 +0530

    learned trie data structure

--- TrieLearning.py
+++ TrieLearning.py
@@ -86,3 +86,109 @@ class Trie(object):
 # obj.insert(word)
 # param_2 = obj.search(word)
 # param_3 = obj.startsWith(prefix)
+
+
+
+#above functions are good if no delete operation is there
+# so if there is also a delete operation then we need to increment then end value by one 
+# since there may be more then one string inserted in the trie 
+# therefore we will use isLast as a number which will increament when the word end at that char
+# and in case of delete we will decrement that isLast value by 1 if it is greater then 0 else will return False 
+# since there is no word ending at this position
+
+
+
+class TrieNode(object):
+    def __init__(self):
+        self.c=''
+        self.children={}
+        self.end=0
+
+
+
+class Trie(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root=None
+        self.root=TrieNode()
+        
+        
+
+    def insert(self, word):
+        """
+        Inserts a word into the trie.
+        :type word: str
+        :rtype: None
+        """
+        #word: apple
+        parent=self.root
+        
+        for i in word:
+            if(not parent.children.get(i)):
+                parent.children[i]=TrieNode()
+            parent=parent.children[i]
+        parent.end+=1
+        
+            
+        
+
+    def search(self, word):
+        """
+        Returns if the word is in the trie.
+        :type word: str
+        :rtype: bool
+        """
+        parent=self.root
+        
+        for i in word:
+            if(not parent.children.get(i)):
+                return False
+            parent=parent.children[i]
+        return parent.end
+    
+    def  delete(self, word):
+        """
+        Returns if the word is in the trie.
+        :type word: str
+        :rtype: bool
+        """
+        parent=self.root
+        
+        for i in word:
+            if(not parent.children.get(i)):
+                return False
+            parent=parent.children[i]
+            
+        if(parent.end==0):
+            #not possible to delete
+            return False
+        else:
+            #reduced by one since there might be dublicate elements
+            parent.end-=1
+            return True
+        
+        
+
+    def startsWith(self, prefix):
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        :type prefix: str
+        :rtype: bool
+        """
+        parent=self.root
+        
+        for i in prefix:
+            if(not parent.children.get(i)):
+                return False
+            parent=parent.children[i]
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
