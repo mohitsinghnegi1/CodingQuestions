332c35e83352b54cef4e05293185a291adc7b448
mohitsinghnegi1 <37727115+mohitsinghnegi1@users.noreply.github.com>
2020-08-15 23:25:01 +0530

    lowest common ansistor

---
+++ LetterCombinationsofaPhoneNumber.py
@@ -0,0 +1,102 @@
+
+
+# QUs:https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+
+#-----Recursive solution
+
+
+def getFull(left,string):
+    out=[]
+    for right in string:
+        out.append(left+right)
+    return out
+    
+def getCombinations(digits,d):
+    #handle case when we get a combination and ther is no digit left fo           
+    #generating combination
+    if(digits==''):
+        return ['']
+    
+    out=[]
+    
+    for c in d[digits[0]]:
+        #add all combination which start from char c and 
+        #combine it with all combination possible with digits[1:] string
+        a=getFull(c,getCombinations(digits[1:],d))
+        out.extend(a)
+    return out
+    
+
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        #if digit is empty there is no combination
+        if(digits==''):
+            return []
+        
+        d={
+            '2':"abc",
+            '3':"def",
+            '4':"ghi",
+            '5':"jkl",
+            '6':"mno",
+            '7':"pqrs",
+            '8':"tuv",
+            '9':"wxyz"
+        }
+        
+        #this function will return list of combination 
+        return getCombinations(digits,d)
+    
+    
+    
+    #-------iterative solution
+    
+      
+
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        #if digit is empty there is no combination
+        if(digits==''):
+            return []
+        
+        d={
+            '2':"abc",
+            '3':"def",
+            '4':"ghi",
+            '5':"jkl",
+            '6':"mno",
+            '7':"pqrs",
+            '8':"tuv",
+            '9':"wxyz"
+        }
+        
+        out=['']
+        
+        for digit in digits:
+            
+            
+            out1=[]
+            for cr in d[digit]:
+                
+                #till now we have sub_comb we need to combine it with 
+                #new cr mapped with digit
+                
+                
+                for k,sub_comb in enumerate(out):
+                    # print "out[k]",out[k]
+                    out1.append(out[k]+cr)
+                # print out1
+            out=out1
+        return out
+            
+            
+        
+       
---
+++ LowestCommonAncestorofaBinaryTree.py
@@ -0,0 +1,99 @@
+# Qus:https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+#-- brute force approch and more space
+
+from collections import defaultdict
+def constructTree(root,p,q,d):
+    if(root==None):
+        return 
+    d[root.left]=root
+    d[root.right]=root
+    constructTree(root.left,p,q,d)
+    constructTree(root.right,p,q,d)
+
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        #construct child-> [parents]
+        d=defaultdict(TreeNode)
+        constructTree(root,p,q,d)
+        
+        #triverse from child to parent to get lowest common Ansestor
+        
+        
+        d[root]=None
+        ptr=p
+        
+        ansistors={}
+        
+        # we can use dict
+        # we need to get all the ansisters of this node p
+        while(ptr):
+            ansistors[ptr]=True
+            ptr=d[ptr]
+        
+        # we need to get ansistors of node q ans as soon as
+        # the ansistore of q available on ansistors dict we need to return it
+        ptr=q
+
+        while(ptr):
+            if(ansistors.get(ptr)!=None):
+                return ptr
+            ptr=d[ptr]
+     
+        
+#better approach without using O(1)space
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+
+
+class Solution(object):
+    def __init__(self):
+        self.ans=None
+        
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        def ansistor(root):
+            if(root==None):
+                return False
+            
+            left=ansistor(root.left)
+            mid=root==p or root==q
+            right=ansistor(root.right)
+
+
+            if(mid+left+right>=2):
+                self.ans=root
+
+            return left or mid or right
+        
+        ansistor(root)
+        return self.ans
+        
+        
+        
+        
