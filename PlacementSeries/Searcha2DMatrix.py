d34c2e080701fe430e642f3ca167107b4fd9949a
mohitsinghnegi1 <37727115+mohitsinghnegi1@users.noreply.github.com>
2020-09-06 00:35:14 +0530

    6 sept 2020

--- PlacementSeries/Searcha2DMatrix.py
+++ PlacementSeries/Searcha2DMatrix.py
@@ -82,3 +82,33 @@ class Solution(object):
             else:
                 b = mid
         return False
+
+
+# --------------------------------------------
+
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        n = len(matrix)
+
+        if(n == 0 or len(matrix[0]) == 0):
+            return False
+        m = len(matrix[0])
+
+        # while there is no out of bound condition
+        i = 0
+        j = m-1
+
+        while(i < n and j >= 0):
+
+            if(matrix[i][j] == target):
+                return True
+            elif(matrix[i][j] > target):
+                j -= 1
+            else:
+                i += 1
+        return False
