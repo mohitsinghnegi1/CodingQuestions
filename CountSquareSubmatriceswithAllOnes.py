dbb5a498a2e4aaa6e9d6dc5e4f8c85dd6557131a
mohitsinghnegi1 <37727115+mohitsinghnegi1@users.noreply.github.com>
2020-09-24 01:48:09 +0530

    24 Sept 2020

--- CountSquareSubmatriceswithAllOnes.py
+++ CountSquareSubmatriceswithAllOnes.py
@@ -29,3 +29,9 @@ class Solution(object):
                     count += matrix[i][j]
         print matrix
         return count
+
+# Follow below syntax to reduct one extra loop
+# for (int i = 0; i < A.size(); ++i):
+#     for (int j = 0; j < A[0].size(); res += A[i][j++]):
+#         if (A[i][j] && i && j):
+#             A[i][j] += min({A[i - 1][j - 1], A[i - 1][j], A[i][j - 1]});
