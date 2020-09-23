cc25dc51f36fe50b66e98b344a9bef31a2ab180f
mohitsinghnegi1 <37727115+mohitsinghnegi1@users.noreply.github.com>
2020-09-24 01:41:16 +0530

    git push origin master

---
+++ CountSquareSubmatriceswithAllOnes.py
@@ -0,0 +1,31 @@
+# Qus: https: // leetcode.com/problems/count-square-submatrices-with-all-ones/
+class Solution(object):
+    def countSquares(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: int
+        """
+        if(len(matrix) == 0 or len(matrix[0]) == 0):
+            return 0
+
+        count = 0
+
+        # first row sum
+        count += sum(matrix[0])
+
+        for i in range(1, len(matrix)):
+            # first column sum
+            count += matrix[i][0]
+
+            for j in range(1, len(matrix[0])):
+
+                if(matrix[i][j] == 1):
+                    # no of possible square that end at point i,j will
+                    # be equal to the min of max possible square out of
+                    # square ending at i-1,j-1 & i-1,j & i,j-1
+
+                    matrix[i][j] = 1+min(matrix[i][j-1],
+                                         matrix[i-1][j], matrix[i-1][j-1])
+                    count += matrix[i][j]
+        print matrix
+        return count
