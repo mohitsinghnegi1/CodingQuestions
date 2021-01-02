0b732839f543b6cf51bb37c0ebc521ca4a3c3f6f
Mohit Singh Negi <mohitsin3232@gmail.com>
2021-01-02 16:43:46 +0530

    2 Jan 2020

--- Stone Game IV.py
+++ Stone Game IV.py
@@ -9,30 +9,30 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        d = {}
+        d = [[-1]*2 for i in range(n+1)]
 
         def canAliceWin(n, turn=1):
 
-            if((n, turn) in d):
-                return d[(n, turn)]
+            if(d[n][turn] != -1):
+                return d[n][turn]
             if(n == 0):
                 # turn of bob return true else return false
                 return not turn
             if(turn):
 
-                d[(n, turn)] = any(canAliceWin(n-i**2, turn ^ 1)
-                                   for i in range(1, int(math.sqrt(n)+1)))
-                return d[(n, turn)]
+                d[n][turn] = any(canAliceWin(n-i**2, turn ^ 1)
+                                 for i in range(1, int(math.sqrt(n)+1)))
+                return d[n][turn]
             else:
                 # any case where alica is not wining
 
                 for i in range(1, int(math.sqrt(n)+1)):
 
                     if(not canAliceWin(n-i**2, turn ^ 1)):
-                        d[(n, turn)] = False
-                        return d[(n, turn)]
-                d[(n, turn)] = True
-                return d[(n, turn)]
+                        d[n][turn] = False
+                        return d[n][turn]
+                d[n][turn] = True
+                return d[n][turn]
 
         ans = canAliceWin(n)
         # print d
