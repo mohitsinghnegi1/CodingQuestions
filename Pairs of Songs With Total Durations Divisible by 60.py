e3c5a82ae59590046d7b2805e021f89fc8a23e2c
Mohit Singh Negi <mohitsin3232@gmail.com>
2021-01-30 20:17:44 +0530

    30 Jan 2021

--- Pairs of Songs With Total Durations Divisible by 60.py
+++ Pairs of Songs With Total Durations Divisible by 60.py
@@ -14,15 +14,15 @@ class Solution(object):
         #           (a % 60 + b % 60) % 60 = 0
         #           (x + y) % 60 = 0  where x and y < 60 for sure
         # since 1 <= time[i] <= 500    we can assure a canot be 0 without modulo
-
+        # see solution at leetcode
         count = 0
 
         for a in time:
             x = a % 60
-            if (60 - x) % 60 in d:
-                # let say a was 60 and x = a % 60 =0    60 - x = 60 will
-                count += d[(60 - x) % 60]
-                # not be in dict for sure , so we take % of it
+
+            # let say a was 60 and x = a % 60 =0    60 - x = 60 will
+            count += d[(60 - x) % 60]
+            # not be in dict for sure , so we take % of it
             d[x] += 1
 
         # print d
