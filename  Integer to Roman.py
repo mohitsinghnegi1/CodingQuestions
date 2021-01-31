977d7056e8d6f164f873495c00234ba8ac4b1879
Mohit Singh Negi <mohitsin3232@gmail.com>
2021-01-31 17:36:27 +0530

    31 Jan 2021

---  Integer to Roman.py
+++  Integer to Roman.py
@@ -45,3 +45,59 @@ class Solution(object):
             # substract the number also
             num = num-r
         return ans
+
+
+# easy way
+class Solution2(object):
+    def intToRoman(self, n):
+        """
+        :type num: int
+        :rtype: str
+        """
+        # add vale of 4 9 ....so on
+        d = {1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L',
+             90: 'XC', 100: 'C', 400: 'CD', 500: 'D', 900: 'CM', 1000: 'M'}
+
+        roman = ''
+        while(n > 0):
+
+            if(n >= 1000):
+                roman += 'M'
+                n -= 1000
+            elif(n >= 900):
+                roman += 'CM'
+                n -= 900
+            elif(n >= 500):
+                roman += 'D'
+                n -= 500
+            elif(n >= 400):
+                roman += 'CD'
+                n -= 400
+            elif(n >= 100):
+                roman += 'C'
+                n -= 100
+            elif(n >= 90):
+                roman += 'XC'
+                n -= 90
+            elif(n >= 50):
+                roman += 'L'
+                n -= 50
+            elif(n >= 40):
+                roman += 'XL'
+                n -= 40
+            elif(n >= 10):
+                roman += 'X'
+                n -= 10
+            elif(n >= 9):
+                roman += 'IX'
+                n -= 9
+            elif(n >= 5):
+                roman += 'V'
+                n -= 5
+            elif(n >= 4):
+                roman += 'IV'
+                n -= 4
+            elif(n >= 1):
+                roman += 'I'
+                n -= 1
+        return roman
