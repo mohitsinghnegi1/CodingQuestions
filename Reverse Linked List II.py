c721be7857a9f2559c2b314329a0ca8f06f85525
Mohit Singh Negi <mohitsin3232@gmail.com>
2021-02-12 00:34:39 +0530

    12 Feb 2021

--- Reverse Linked List II.py
+++ Reverse Linked List II.py
@@ -1,10 +1,10 @@
 # Qus: https: // leetcode.com/problems/reverse-linked-list-ii/
 
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
 class Solution(object):
