46033bdfc8a5ef85f9a87dc4fe81259ad021547a
Mohit Singh Negi <mohitsin3232@gmail.com>
2021-04-18 00:31:34 +0530

    18 April 2021

--- Flatten Binary Tree to Linked List.py
+++ Flatten Binary Tree to Linked List.py
@@ -47,3 +47,33 @@ class Solution(object):
             node.right = None
             ptr1.right = node
             ptr1 = ptr1.right
+
+
+# easy to understand iterative approach  also rember how we do preorder triversal using stack
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution3(object):
+    def flatten(self, root):
+        """
+        :type root: TreeNode
+        :rtype: None Do not return anything, modify root in-place instead.
+        """
+        if(root == None):
+            return root
+        stack = []
+        stack.append(root)
+        while(stack):
+            node = stack.pop()
+            if(node.right):
+                stack.append(node.right)
+            if(node.left):
+                stack.append(node.left)
+            if(stack):
+                # whatever will be processed next will be the right child of current poped node
+                node.right = stack[-1]
+                node.left = None
+        return root
