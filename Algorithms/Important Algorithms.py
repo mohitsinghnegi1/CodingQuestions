e15b3daba8a7da94c5ca1289f016b40423df5c18
Mohit Singh Negi <mohitsin3232@gmail.com>
2021-05-09 00:28:43 +0530

    9 May 2021

--- Algorithms/Important Algorithms.py
+++ Algorithms/Important Algorithms.py
@@ -45,6 +45,25 @@
 
 # Why Amazon, Why leaving current organization in 6 months, What do you bring to the table?
 
+********** AMAZON LEADERSHIP PRINICIPLES QUESTION ** *****************
+# Tell me about yourself.
+# Tell me about current company(team) and your role in it
+# What are you looking for in next company
+# Why do you want to leave your currrent company
+# Amazon Leadership Principles
+# Tell me an area where you think you can improve.
+# Tell me a time when you created an innovative product
+# Give me an example of when you took a risk and it failed.
+# Walk us through a time when you helped a customer through a difficult process and what that looked like.
+# Tell me about a time when had conflict with your manager/team member.
+# Tell me about a time when you could have stopped working but persisted.
+# Tell me about a time when you received critical feedback from someone.
+# Tell me about a time when you had to deal with a poor performer on your team.
+# Tell me about a time where you overcame an obstacle and delivered results.
+# Tell me about one of your failures/mistakes.
+# How do you prioritize different tasks coming to the team?
+# Tell me about a time when you brought a change by asking questions
+
 
 # Paging / Virtual Memory
 # CPU Scheduling Algorithm
@@ -93,7 +112,7 @@
 # https: // www.geeksforgeeks.org/find-next-greater-number-set-digits/
 # https://www.geeksforgeeks.org/find-missing-element-in-a-sorted-array-of-consecutive-numbers/?ref=rp
 # https://www.geeksforgeeks.org/find-itinerary-from-a-given-list-of-tickets/
-# https://leetcode.com/problems/rotting-oranges/
+# https://leetcode.com/problems/rotting-oranges/                                        --done
 # https://www.geeksforgeeks.org/find-all-possible-trees-with-given-inorder-traversal/   --done
 # https://www.geeksforgeeks.org/program-count-numbers-fingers/                          --done
 # https://www.geeksforgeeks.org/python-converting-all-strings-in-list-to-integers/      --done
