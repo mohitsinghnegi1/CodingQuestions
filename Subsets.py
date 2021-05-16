a57507c3dc25a58a48b9e86a8cd8dd07e6bf1fdd
Mohit Singh Negi <mohitsin3232@gmail.com>
2021-05-16 14:22:20 +0530

    16 May 2021

--- Subsets.py
+++ Subsets.py
@@ -19,3 +19,34 @@ class Solution(object):
 
         getAllSubset(nums)
         return out
+
+
+# iterative method to find all subset using bit manipuation vvv imp
+
+"""
+    Intution : we know there are total 2*n-1 number of subset
+    so we need to treverse from 1 to 2n-1 and for every i we need to pick only those element in array where the jth bit of ith elemetn is set
+"""
+
+
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        n = len(nums)
+        res = []
+
+        # there are total 2**(n-1) number of subset
+        for bi in range(2**(n)):
+
+            ss = []
+            for i in range(len(nums)):  # len bit in binary
+
+                if(bi & 1 << i):
+                    # if ith bit is one then we need to take it else not
+                    ss.append(nums[i])
+            # print ss
+            res.append(ss)
+        return res
