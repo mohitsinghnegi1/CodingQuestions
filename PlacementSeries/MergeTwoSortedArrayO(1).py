9b3aff51505f2d20a8a9d0326b721d3162e81cf3
Mohit Singh Negi <mohitsin3232@gmail.com>
2021-05-20 03:26:38 +0530

    20 May 2021

--- PlacementSeries/MergeTwoSortedArrayO(1).py
+++ PlacementSeries/MergeTwoSortedArrayO(1).py
@@ -2,6 +2,9 @@
 # resource: https: // www.youtube.com/watch?v = hVl2b3bLzBw & list = PLgUwDviBIf0rPG3Ictpu74YWBQ1CaBkm2 & index = 4
 
 
+import math
+
+
 def nextGap(gap):
     if(gap == 1):
         return 0
@@ -52,3 +55,53 @@ for i in range(t):
         print(i, end=' ')
 
     print()
+
+
+# next revision
+# User function Template for python3
+# time complexity O(n)*nlog(n+m)
+
+class Solution:
+
+    # Function to merge the arrays.
+    def merge(self, arr1, arr2, n, m):
+        # code here
+
+        n = len(arr1)
+        m = len(arr2)
+        gap = (n+m)//2 + (n+m) % 2 if (n+m) > 1 else 0
+        while(gap):
+            i = 0
+            while(i+gap < n+m):
+                a = b = None
+                if(i+gap < n):
+                    # means both lie in same side
+                    if(arr1[i] > arr1[i+gap]):
+                        arr1[i], arr1[i+gap] = arr1[i+gap], arr1[i]
+
+                # if both lie in different side
+                if(i < n and i+gap >= n):
+                    if(arr1[i] > arr2[i+gap-n]):
+                        arr1[i], arr2[i+gap-n] = arr2[i+gap-n], arr1[i]
+                if(i >= n):
+                    if(arr2[i-n] > arr2[i+gap-n]):
+                        arr2[i-n], arr2[i+gap-n] = arr2[i+gap-n], arr2[i-n]
+                i += 1
+            gap = gap//2+(gap % 2) if gap > 1 else 0  # ceil
+            # print(gap)
+
+
+# {
+#  Driver Code Starts
+# Initial template for Python
+if __name__ == '__main__':
+    t = int(input())
+    for tt in range(t):
+        n, m = map(int, input().strip().split())
+        arr1 = list(map(int, input().strip().split()))
+        arr2 = list(map(int, input().strip().split()))
+        ob = Solution()
+        ob.merge(arr1, arr2, n, m)
+        print(*arr1, end=" ")
+        print(*arr2)
+# } Driver Code Ends
