59220c558bef04cd91935e17d5e3a8d3c86ceb5b
Mohit Singh Negi <mohitsin3232@gmail.com>
2021-08-08 13:43:46 +0530

    8 Aug 2021

--- Palindrome Partitioning II.py
+++ Palindrome Partitioning II.py
@@ -2,6 +2,9 @@
 # understood by pepcoding. gap method + defination of palindrom abc if a==c then our ans is isPalin(b)
 
 # Recursive solution (TLE) # we can optimise a bit by using a isPalindrom map (construct using gap method)
+import sys
+
+
 class Solution(object):
     def minCut(self, s):
         """
@@ -131,3 +134,76 @@ class Solution3(object):
             return dp[-1]
 
         return findMinCut(s)
+
+
+# easy intution using recursion
+
+# ****** gap stategy ******
+
+# given a string. find whether the given substring from i,j is palindrom or not
+# dp[i][j] stores ans of string starting from index i to index j
+
+# Note : We can store ans of reverse string just dp[i][j] = d[j][j] in case of palindrome
+# for other cases we genrally not fill those but if we store those cells that means it will store ans of greate index to smaller index ans
+
+# Note : Here dp[i][j] depends on dp[i+1][j-1] , hence we need to compute value of i+1 row value and col -1 value first
+# so we need to create a loop from last to first index and col index from first to last
+
+
+def createIsPalindromDp(s):
+    n = len(s)
+
+    dp = [[2]*n for _ in range(n)]
+
+    for i in range(n-1, -1, -1):
+        # print "row ", i
+        for j in range(i, n):
+            gap = j-i
+            # print gap
+
+            if(gap == 0):
+                # only one character - always palindrom
+                dp[i][j] = 1
+            elif(gap == 1):
+                # two characters - check if first and last char is same
+                if(s[i] == s[j]):
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = 0
+            else:
+                # print i, j, s[i], s[j], dp[i+1][j-1]
+                dp[i][j] = 1 if (s[i] == s[j] and dp[i+1][j-1]) else 0
+    return dp
+
+
+class Solution(object):
+    def minCut(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        isPalin = createIsPalindromDp(s)
+
+        d = [-1]*n
+
+        def findMinCut(i, n):  # stores ans of suffix s[0..n-1]
+
+            if(d[i] != -1):
+                return d[i]
+
+            minCut = sys.maxsize
+
+            for j in range(i, n):
+                # make a cut after i..j |)
+                if(isPalin[i][j] and j == n-1):
+                    # if palin then we can make a cut next to the last element
+                    minCut = 0
+                elif(isPalin[i][j]):
+                    minCut = min(minCut, 1+findMinCut(j+1, n))
+
+            d[i] = minCut
+
+            return d[i]
+
+        return findMinCut(0, n)
---
+++ ProblemSolvingStategies/gapStategy.py
@@ -0,0 +1,40 @@
+# ****** gap stategy ******
+
+# given a string. find whether the given substring from i,j is palindrom or not
+# dp[i][j] stores ans of string starting from index i to index j
+
+# Note : We can store ans of reverse string just dp[i][j] = d[j][j] in case of palindrome
+# for other cases we genrally not fill those but if we store those cells that means it will store ans of greate index to smaller index ans
+
+# Note : Here dp[i][j] depends on dp[i+1][j-1] , hence we need to compute value of i+1 row value and col -1 value first
+# so we need to create a loop from last to first index and col index from first to last
+
+def createIsPalindromDp(s):
+    n = len(s)
+
+    dp = [[2]*n for _ in range(n)]
+
+    for i in range(n-1, -1, -1):
+        print "row ", i
+        for j in range(i, n):
+            gap = j-i
+            print gap
+
+            if(gap == 0):
+                # only one character - always palindrom
+                dp[i][j] = 1
+            elif(gap == 1):
+                # two characters - check if first and last char is same
+                if(s[i] == s[j]):
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = 0
+            else:
+                print i, j, s[i], s[j], dp[i+1][j-1]
+                dp[i][j] = 1 if (s[i] == s[j] and dp[i+1][j-1]) else 0
+    return dp
+
+
+dp = createIsPalindromDp('abcbb')
+for row in dp:
+    print row
